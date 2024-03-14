80b4a66d3b734ea04681ef133194db5be5d23e08
Jetistrim <paulogabriellima@hotmail.com>
2024-03-14 12:06:35 -0300

    updade images

--- js/index.js
+++ js/index.js
@@ -13,29 +13,33 @@ function getCategories() {
 
 
 async function getProducts() {
-    const request = await fetch('https://fakestoreapi.com/products');
-    const response = await request.json();
-    if (response.length > 0){
-        response.map(produto => {
-            produtos.innerHTML += `<li>
-            <img src="/Image/image${produto.id}.png" alt="">
-            <div class="tituloItem">
-                <h3>${produto.title}</h3>
-            </div>
-            <div class="categoriaItem">
-                <h4>${produto.category}</h4>
-            </div>
-            <div class="precoAvaliacao">
-                <span class="preco">R$ ${produto.price}</span>
-                <div class="avaliacao">
-                <span>&#x2B50 &#x2B50 &#x2B50 &#x2B50 &#x2B50</span>
-                <span> ${produto.rating.rate}/5.0</span>
+    try {
+        const request = await fetch('https://fakestoreapi.com/products');
+        const response = await request.json();
+        produtos.innerHTML = "";
+        if (response.length > 0){
+            response.map(produto => {
+                produtos.innerHTML += `<li>
+                <img src="${produto.image}" alt="">
+                <div class="tituloItem">
+                    <h3>${produto.title}</h3>
                 </div>
-            </div>
-        </li>`
-        })
-        return;
+                <div class="categoriaItem">
+                    <h4>${produto.category}</h4>
+                </div>
+                <div class="precoAvaliacao">
+                    <span class="preco">R$ ${produto.price.toFixed(2)}</span>
+                    <div class="avaliacao">
+                    <span>&#x2B50 &#x2B50 &#x2B50 &#x2B50 &#x2B50</span>
+                    <span> ${produto.rating.rate}/5.0</span>
+                    </div>
+                </div>
+            </li>`
+            })
     }
-    alert('produtos não encontrados :/');
+        return;
 
+    } catch (error){
+        alert("produtos não encontrados")
+        }
 } getProducts();
