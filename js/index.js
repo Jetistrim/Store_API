a484033f7af74dfbfa7ad67526b2ed2b3e09914a
Jetistrim <paulogabriellima@hotmail.com>
2024-03-21 11:54:37 -0300

    update + bug fixes

--- js/index.js
+++ js/index.js
@@ -1,60 +1,80 @@
+let lista = [];
+
 function getCategories() {
     fetch('https://fakestoreapi.com/products/categories')
             .then(response=>response.json())
             .then(response=>{
-                categorias.innerHTML += `<option hidden>-- Filtrar Por --</option>`
+                categorias.innerHTML += `<option hidden>-- Filtrar Por --</option>
+                <option>todos</option>`
                 for (let i = 0; i < response.length; i++) {
                     categorias.innerHTML += `<option>${response[i]}</option>`;
                 }
             })
 } getCategories();
 
-function searchProductByCategory(){
-    fetch(`https://fakestoreapi.com/products/category/${categorias.value}`)
-        .then(response=>response.json())
-        .then(response=>{
-            console.log(response);
-            for (let i = 0; i < produtos.children.length; i++){
-                if (response[i].category.value != categorias.value){
-                    console.log(response[i].category)
-                    let elemento = document.querySelector('main ul li')
-                    elemento.classList.add('none')
-                }            
-            }
-        })
+// function searchProductByCategory(){
+//     fetch(`https://fakestoreapi.com/products/category/${categorias.value}`)
+//         .then(response=>response.json())
+//         .then(response=>{
+//             console.log(response);
+//             for (let i = 0; i < produtos.children.length; i++){
+//                 if (response[i].category.value != categorias.value){
+//                     console.log(response[i].category)
+//                     let elemento = document.querySelector('main ul li')
+//                     elemento.classList.add('none')
+//                 }            
+//             }
+//         })
+// }
+
+function orderingBy(){
+    renderList(lista.toSorted((a, b) => ordenacao.value ? a.price - b.price : a.rating.rate - b.rating.rate))
+}
+
+function filteredBy(){
+    if (categorias.value == "todos"){
+        renderList(lista)
+    } else {
+        renderList(lista.filter((item) => item.category == categorias.value))
+    }
 }
 
 async function getProducts() {
     try {
         const request = await fetch('https://fakestoreapi.com/products');
         const response = await request.json();
-        produtos.innerHTML = "";
+        lista = response
         if (response.length > 0){
-            response.map(produto => {
-                produtos.innerHTML += `<li value=${produto.id}>
-                <img src="${produto.image}" alt="${produto.title}">
-                <div class="tituloItem">
-                    <h3>${produto.title}</h3>
-                </div>
-                <div class="categoriaItem">
-                    <h4>${produto.category}</h4>
-                </div>
-                <div class="precoAvaliacao">
-                    <span class="preco">R$ ${produto.price.toFixed(2)}</span>
-                    <div class="avaliacao">
-                    <span>&#x2B50 &#x2B50 &#x2B50 &#x2B50 &#x2B50</span>
-                    <span> ${produto.rating.rate}/5.0</span>
-                    </div>
-                </div>
-            </li>`
-            })
-    }
+        renderList(response)
+        }
         return;
 
     } catch (error){
         alert("produtos não encontrados")
         }
 } getProducts();
+
+function renderList(arr){
+    produtos.innerHTML = "";
+    arr.map(produto => {
+        produtos.innerHTML += `<li value=${produto.id}>
+        <img src="${produto.image}" alt="${produto.title}">
+        <div class="tituloItem">
+            <h3>${produto.title}</h3>
+        </div>
+        <div class="categoriaItem">
+            <h4>${produto.category}</h4>
+        </div>
+        <div class="precoAvaliacao">
+            <span class="preco">R$ ${produto.price.toFixed(2)}</span>
+            <div class="avaliacao">
+            <span>&#x2B50 &#x2B50 &#x2B50 &#x2B50 &#x2B50</span>
+            <span> ${produto.rating.rate}/5.0</span>
+            </div>
+        </div>
+    </li>`
+    })
+}
 
 function searchProductByName(){
     fetch('https://fakestoreapi.com/products')
