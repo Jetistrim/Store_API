5a3d984d2d6aa71c9d2e3035aefa1d1ebc222f6d
Paulo Gabriel Lima de Sousa <paulogabriellima@hotmail.com>
2024-03-20 22:53:21 -0300

    update

--- js/index.js
+++ js/index.js
@@ -2,15 +2,43 @@ function getCategories() {
     fetch('https://fakestoreapi.com/products/categories')
             .then(response=>response.json())
             .then(response=>{
+                categorias.innerHTML += `<option hidden>-- Filtrar Por --</option>`
                 for (let i = 0; i < response.length; i++) {
                     categorias.innerHTML += `<option>${response[i]}</option>`;
                 }
-                // response.map(r => {
-                //     categorias.innerHTML += `<option>${r}</option>`
-                // })
             })
 } getCategories();
 
+function searchProductByCategory(){
+                fetch(`https://fakestoreapi.com/products/category/${categorias.value}`)
+                .then(response=>response.json())
+                .then(response=>{
+                    console.log(response);
+                    response.map(e => {
+                        if (e.category.value != categorias.value){
+                            console.log(e.category)
+                            let elemento = document.querySelector('main ul li')
+                            elemento.classList.add('none')
+                        }
+                        return;
+                    })
+                    //     switch (categoria){
+                    //         case 0:
+                    //             document.querySelector('main ul li').classList.remove('none')
+                    //             document.querySelector('main ul li .categoriaItem h3').innerText.includes('eletronics').classList.add('none')
+                    //         case 1:
+                    //             document.querySelector('main ul li').classList.remove('none')
+                    //             document.querySelector('main ul li .categoriaItem h3').innerText.includes('jewelery').classList.add('none')
+                    //         case 2:
+                    //             document.querySelector('main ul li').classList.remove('none')
+                    //             document.querySelector('main ul li .categoriaItem h3').innerText.includes("men's clothing").classList.add('none')
+                    //         case 3:
+                    //             document.querySelector('main ul li').classList.remove('none')
+                    //             document.querySelector('main ul li .categoriaItem h3').innerText.includes("women's clothing").classList.add('none')
+                    //     }
+
+                })
+}
 
 async function getProducts() {
     try {
@@ -19,8 +47,8 @@ async function getProducts() {
         produtos.innerHTML = "";
         if (response.length > 0){
             response.map(produto => {
-                produtos.innerHTML += `<li>
-                <img src="${produto.image}" alt="">
+                produtos.innerHTML += `<li value=${produto.id}>
+                <img src="${produto.image}" alt="${produto.title}">
                 <div class="tituloItem">
                     <h3>${produto.title}</h3>
                 </div>
@@ -43,3 +71,20 @@ async function getProducts() {
         alert("produtos não encontrados")
         }
 } getProducts();
+
+function searchProductByName(){
+    fetch('https://fakestoreapi.com/products')
+            .then(response=>response.json())
+            .then(response=>{
+                categorias.innerHTML += `<option hidden>-- Filtrar Por --</option>`
+                if (busca.value.length >= 3){
+                    for (let i = 0; i < response.length; i++) {
+                        if(response[i].title.toString().toLowerCase().includes(busca.value.toLowerCase())){
+                            document.querySelector('main ul li').classList.remove('none')
+                        } else {
+                            document.querySelector('main ul li').classList.add('none')
+                        };
+                    };
+                };
+            });
+    } //QUEBRADO
